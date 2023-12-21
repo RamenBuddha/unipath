a8d6a51bf91105fda269b412adc7bee6bcb8bdeb
RamenBuddha <changs@ufl.edu>
2023-12-20 22:09:20 -0500

    made a map class to construct a graph out of a csv of buildings and coordinates

--- nodes.py
+++ nodes.py
@@ -2,11 +2,32 @@ import networkx as nx
 import pandas as pd
 from geopy import distance
 
-def graphMaker(filename):
-    data = pd.read_csv('building.csv',sep=',')
-    map = nx.Graph()
-    for index, data1 in data.iterrows():
-        for index2, data2 in data.iterrows():
-            map.add_edge(data1.at["Building"],data2.at["Building"], weight=distance.distance((data1.at["Latitude"], data1.at["Longitude"]),(data2.at["Latitude"], data2.at["Longitude"])).miles)
-    for first, second, attr in map.edges(data=True):
-        print(f"{first} to {second} with weight {attr.get('weight')}")
+class Map:
+    def __init__(self) -> None:
+        self.map = self.graphMaker("building.csv")
+        self.mst = nx.minimum_spanning_tree(self.map)
+        self.maxst = nx.maximum_spanning_tree(self.map)
+
+    def getMap(self):
+        return self.map
+    
+    def getMST(self):
+        return self.mst
+    
+    def getMaxST(self):
+        return self.maxst
+
+    def graphMaker(self, filename):
+        visited = set()
+        data = pd.read_csv(filename,sep=',')
+        map = nx.Graph()
+        for index, data1 in data.iterrows():
+            for index2, data2 in data.iterrows():
+                if data1.at["Building"] == data2.at["Building"]:
+                    continue
+                if tuple([data1["Building"],data2["Building"]]) in visited:
+                    continue
+                map.add_edge(data1.at["Building"],data2.at["Building"], weight=distance.distance((data1.at["Latitude"], data1.at["Longitude"]),(data2.at["Latitude"], data2.at["Longitude"])).miles)
+                visited.add(tuple([data1.at["Building"],data2.at["Building"]]))
+            nx.set_node_attributes(map,{data1.at["Building"]: {'Latitude': data1.at["Latitude"], 'Longitude': data1.at["Longitude"]}})
+        return map
