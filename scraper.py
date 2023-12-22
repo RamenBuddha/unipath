a278d04cda17ab547b25317a8c4c384077f83dd4
RamenBuddha <changs@ufl.edu>
2023-12-22 01:13:07 -0500

    can scrape off other wikipedia pages now, still testing

--- scraper.py
+++ scraper.py
@@ -3,22 +3,36 @@ import requests
 import pandas as pd
 from geopy.geocoders import Nominatim
 
-wiki_url = 'https://en.wikipedia.org/wiki/List_of_University_of_Florida_buildings'
+def scrape(url, city=""):
+    wiki_url = url
+    print(wiki_url)
 
-response = requests.get(wiki_url)
-soup = BeautifulSoup(response.text,'html.parser')
+    response = requests.get(wiki_url)
+    soup = BeautifulSoup(response.text,'html.parser')
 
-building_list = soup.find('table', attrs={'class': "wikitable sortable"})
-df = pd.read_html(str(building_list))[0].loc[:,'Building[2]']
+    synonyms = ['Building','Building[2]','Structure','Name']
 
+    building_list = soup.find('table', attrs={'class': "wikitable sortable"})
 
-geolocator = Nominatim(user_agent='unipath',timeout=None)
+    df = None
 
-file = open("building.csv", "w")
-file.write("Building,Latitude,Longitude\n")
-for item in df:
-    building = item + " Gainesville Florida"
-    address=geolocator.geocode(building)
-    if address is not None:
-        file.write(building + "," + str(address.latitude) + "," + str(address.longitude)+ "\n")
-file.close()
+    for synonym in synonyms:
+        try:
+            df = pd.read_html(str(building_list))[0].loc[:,synonym]
+            if df is not None:
+                break
+        except:
+            pass
+
+    if df is None:
+        return False
+    geolocator = Nominatim(user_agent='unipath',timeout=None)
+    file = open("building.csv", "w")
+    file.write("Building,Latitude,Longitude\n")
+    for item in df:
+        building = item
+        address=geolocator.geocode(building + " " + city)
+        if address is not None:
+            file.write(building + "," + str(address.latitude) + "," + str(address.longitude)+ "\n")
+    file.close()
+    return True
